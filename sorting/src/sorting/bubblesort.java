7170a666e9204068c84074fb190eab23b1d4dcb4
student <student@10.14.0.194>
2017-08-23 09:33:36 +0530

    bubblesort

---
+++ sorting/src/sorting/bubblesort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+public class bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int arr[] ={43,54,100,987,55,76};
+      int n=arr.length;
+      for (int i=0;i<n-1;i++){
+    	  for(int j=0;j<n-i-1;j++){
+    		 if(arr[j]>arr[j+1])
+    		 {
+    			 int temp=arr[j];
+    			 arr[j]=arr[j+1];
+    			 arr[j+1]=temp;
+    					 
+    		 }
+    	  }
+      }
+      for(int i=0;i<n;i++){
+    	  System.out.print(arr[i]+" ");
+    	  System.out.println();
+    	  
+      }
+	}
+
+}
